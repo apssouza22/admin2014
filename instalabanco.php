619fe1b9e4548a10c95ef505d23486ab3ceb14c6
Alexsandro Pereira <apssouza22@gmail.com>
2014-04-28 15:53:53 -0300

    create file that create table admin_user

--- instalabanco.php
+++ instalabanco.php
@@ -1,66 +1,19 @@
 <?php
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 'On');
-ini_set('display_startup_errors', 'On');
 
-use Respect\Relational\Db;
-use Respect\Relational\Mapper;
-se();
-require_once './vendor/autoload.php';
+require_once './App/Config/bootstrap.php';
 
-$connection = new PDO('sqlite:datasources/db.sq3');
-$db         = new Db($connection);
-$mapper     = new Mapper($db);
-
-$db->createTable('authors', [
+$db->createTable('admin_user', array(
 	'id    INTEGER PRIMARY KEY',
-	'name  VARCHAR(32)',
-	'email TEXT'
-])->exec();
+	'name varchar(200) NOT NULL',
+	'email varchar(200) NOT NULL',
+	'password varchar(200) NOT NULL',
+	'picture varchar(100) DEFAULT NULL',
+	'role varchar(50) NOT NULL',
+	'salt varchar(50) DEFAULT NULL',
+	'dateinsert timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP',
+	'visible tinyint(1) NOT NULL DEFAULT \'1\'',
+))->exec();
 
-$db->createTable('articles', [
-	'id         INTEGER PRIMARY KEY',
-	'authors_id INTEGER REFERENCES authors (id)',
-	'title      VARCHAR(32)',
-	'text       TEXT'
-])->exec();
 
-$gaigalas = (object) [
-	'id'    => null, // id should be null for new objects to be persisted
-	'name'  => 'Alexandre Gaigalas',
-	'email' => 'alexandre@gaigalas.net'
-];
 
-$john = (object) [
-	'id'    => null, 
-	'name'  => 'John Doe',
-	'email' => 'john@example.com'
-];
 
-$about = (object) [
-	'id'         => null,
-	'authors_id' => $gaigalas, // You can pass the entire object
-	'title'      => 'About',
-	'text'       => 'Abou this.'
-];
-
-$hello = (object) [
-	'id'         => null,
-	'authors_id' => $gaigalas, // You can pass the entire object
-	'title'      => 'Hello',
-	'text'       => 'Hello World.'
-];
-
-// Persist John first
-$mapper->authors->persist($john);
-
-// Gaigalas will be persisted with $articleAndAuthor
-
-// Handles an article with an author inside.
-$articleAndAuthor = $mapper->articles($mapper->authors);
-$articleAndAuthor->persist($about);
-$articleAndAuthor->persist($hello);
-
-$mapper->flush(); // Mapper must be flushed to persist operations
-
-print_r($articleAndAuthor->fetchAll());
