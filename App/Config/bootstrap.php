0a185236bf4663daa7bdbed58c1a50fd146c73e5
Alexsandro Souza <apssouza22@gmail.com>
2014-05-28 02:20:58 +0000

    organizing the bootstrap

--- App/Config/bootstrap.php
+++ App/Config/bootstrap.php
@@ -14,24 +14,21 @@ $dbconfig = array(
 		);
 
 $enviroment = 'dev';
+$sitePath = '/';
 
 if (mb_strpos($_SERVER['HTTP_HOST'], 'com') !== false) {
     $enviroment = 'prod';
-}else{
-    $enviroment = 'dev';
 }
 
-define('ENVIROMENT', $enviroment );
-$sitePath = '/';
-
 if (mb_strpos($_SERVER['HTTP_HOST'], 'localhost') !== false) {
-	$sitePath = '/Meusprojetos/opensource/admin2014/';	
+	$sitePath = '/opensource/admin2014/';	
 	//$sitePath = '/free/admin/';	
 }
 
 $host = $_SERVER['HTTP_HOST'] . '/';
 $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https://' : 'http://';
 
+define('ENVIROMENT', $enviroment );
 define('DIR_ROOT', $_SERVER['DOCUMENT_ROOT'] . $sitePath);
 define('DIR_HTM_ROOT', $protocol . $host . $sitePath);
 
@@ -40,8 +37,8 @@ $loader->add('App', DIR_ROOT);//add path to namespace App
 
 use Respect\Relational\Db;
 
-//$pdo = new PDO("mysql:host=localhost; dbname=testeadmin; ", 'root', '5834');
-$pdo = new PDO("mysql:host={$dbconfig['hostname']}; dbname={$dbconfig['dbname']}; ", "{$dbconfig['username']}","{$dbconfig['password']}");
+$pdo = new PDO("mysql:host=localhost; dbname=testeadmin; ", 'root', '5834');
+//$pdo = new PDO("mysql:host={$dbconfig['hostname']}; dbname={$dbconfig['dbname']}; ", "{$dbconfig['username']}","{$dbconfig['password']}");
 $pdo->exec("set names utf8");
 $pdo->setAttribute(1002, 'SET NAMES utf8');
 
