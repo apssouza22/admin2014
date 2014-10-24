9fda49c8f261a0eb9d27fa0784f3fe9cdf1681f4
Alexsandro Souza <apssouza22@gmail.com>
2014-10-24 12:36:04 -0200

    ajustes no header menu mensagem e alteras

--- admin/includes/menu-header.php
+++ admin/includes/menu-header.php
@@ -37,17 +37,33 @@
 
         <ul class="nav pull-right">
              <li class="dropdown messages-dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> Messages <span class="badge">7</span> <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                    <li class="dropdown-header">7 New Messages</li>
-                    <li class="message-preview">
-                        <a href="#">
-                            <span class="avatar"><img src="http://placehold.it/50x50"></span>
-                            <span class="name">John Smith:</span>
-                            <span class="message">Hey there, I wanted to ask you something...</span>
-                            <span class="time"><i class="fa fa-clock-o"></i> 4:34 PM</span>
-                        </a>
-                    </li>
+                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="Mensagens"><i class="icon-envelope"></i>  <span class="badge">7</span> <b class="caret"></b></a>
+                <ul class="dropdown-menu dropdown-menu-msg">
+                    <li class="dropdown-header"><strong>7 New Messages</strong></li>
+                    <li class="divider"></li>
+                    <li>
+                            <a href="#">
+                                <div>
+                                    <strong>John Smith</strong>
+                                    <span class="pull-right text-muted">
+                                        <em>Yesterday</em>
+                                    </span>
+                                </div>
+                                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
+                            </a>
+                        </li>
+                    <li class="divider"></li>
+                    <li>
+                            <a href="#">
+                                <div>
+                                    <strong>John Smith</strong>
+                                    <span class="pull-right text-muted">
+                                        <em>Yesterday</em>
+                                    </span>
+                                </div>
+                                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
+                            </a>
+                        </li>
                     <li class="divider"></li>
                     <li class="message-preview">
                         <a href="#">
@@ -58,20 +74,11 @@
                         </a>
                     </li>
                     <li class="divider"></li>
-                    <li class="message-preview">
-                        <a href="#">
-                            <span class="avatar"><img src="http://placehold.it/50x50"></span>
-                            <span class="name">John Smith:</span>
-                            <span class="message">Hey there, I wanted to ask you something...</span>
-                            <span class="time"><i class="fa fa-clock-o"></i> 4:34 PM</span>
-                        </a>
-                    </li>
-                    <li class="divider"></li>
-                    <li><a href="#">View Inbox <span class="badge">7</span></a></li>
+                    <li><a href="#"><strong>View Inbox <span class="badge">7</span></strong></a></li>
                 </ul>
             </li>
             <li class="dropdown alerts-dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bell"></i> Alerts <span class="badge">3</span> <b class="caret"></b></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="Alertas"><i class="icon-bell"></i> <span class="badge">3</span> <b class="caret"></b></a>
                 <ul class="dropdown-menu">
                     <li><a href="#">Default <span class="label label-default">Default</span></a></li>
                     <li><a href="#">Primary <span class="label label-primary">Primary</span></a></li>
@@ -84,7 +91,7 @@
                 </ul>
             </li>
             <li class="dropdown">
-                <a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-user"></i><?php echo $_SESSION['userAuth']->name ?><i class="caret"></i>
+                <a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-user"></i> <?php echo $_SESSION['userAuth']->name ?><i class="caret"></i>
 
                 </a>
                 <ul class="dropdown-menu">
